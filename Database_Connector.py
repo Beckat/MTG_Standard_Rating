4570ba3fd81d4e57f324cc33cf01d12bf51ba047
danthom1704 <freddrickmacguffin@gmail.com>
2021-07-25 22:09:39 -0400

    Added an Env.py file to hold login information and added some test code that works with the PostGres SQL

--- Database_Connector.py
+++ Database_Connector.py
@@ -1,1 +1,14 @@
-#HoldingS
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, String, MetaData
+import Env
+
+
+Environment = Env.Environment()
+engine = Environment.get_database_engine()
+
+meta = MetaData(engine)
+engine.execute("CREATE TABLE IF NOT EXISTS films (title text, director text, year text)")
+engine.execute("INSERT INTO films (title, director, year) VALUES ('Test', 'Scott Test', '2017')")
+
+
+print(meta)
