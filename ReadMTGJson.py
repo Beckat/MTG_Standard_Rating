2059baa9e78b12386dc80e6d98205b7f9d87294f
danthom1704 <freddrickmacguffin@gmail.com>
2021-08-03 23:51:15 -0400

    Previous way of getting color didn't handle hybrid costs, just going to use direct JSON

--- ReadMTGJson.py
+++ ReadMTGJson.py
@@ -14,14 +14,6 @@ class ReadCards:
     def __init__(self):
         self.card = None
 
-    def find_mana_cost_symbol(self, symbol, mana_costs):
-        cost = ""
-        if symbol in mana_costs:
-            for x in mana_costs:
-                if x == symbol:
-                    cost = cost + symbol
-        return cost
-
 
 class Card(Base):
     __tablename__ = 'MTG Cards'
@@ -52,46 +44,9 @@ mana_cost = ""
 
 all_cards_export = pd.read_json("StandardAtomic.json", orient = "index")
 
-for card in data["data"]:
-    test = card
-    test_card = data["data"][card]
-    colors = ""
-    #print(test_card[0]["name"])
-    if len(test_card[0]["colors"]) == 0:
-        colors = ""
-    else:
-        if "W" in str(test_card[0]["colors"]):
-            colors = colors + "W"
-        if "U" in str(test_card[0]["colors"]):
-            colors = colors + "U"
-        if "B" in str(test_card[0]["colors"]):
-            colors = colors + "B"
-        if "R" in str(test_card[0]["colors"]):
-            colors = colors + "R"
-        if "G" in str(test_card[0]["colors"]):
-            colors = colors + "G"
-    #print(colors)
-    colors = ""
-
-if len(data["data"]["Angel of Destiny"][0]["manaCost"]) == 0:
-    mana_cost = ""
-else:
-    mana_cost_holder = str(data["data"]["Angel of Destiny"][0]["manaCost"])
-    mana_cost_holder = mana_cost_holder.replace("{", "")
-    mana_costs = mana_cost_holder.split("}")
-    if mana_costs[0].isnumeric():
-        mana_cost = mana_cost + mana_costs[0]
-
-    mana_cost = mana_cost + card_reader.find_mana_cost_symbol("W", mana_costs)
-    mana_cost = mana_cost + card_reader.find_mana_cost_symbol("U", mana_costs)
-    mana_cost = mana_cost + card_reader.find_mana_cost_symbol("B", mana_costs)
-    mana_cost = mana_cost + card_reader.find_mana_cost_symbol("R", mana_costs)
-    mana_cost = mana_cost + card_reader.find_mana_cost_symbol("G", mana_costs)
-    mana_cost = mana_cost + card_reader.find_mana_cost_symbol("C", mana_costs)
-
 ruin_crab = data["data"]["Angel of Destiny"][0]["name"]
 
-card = Card(name=ruin_crab, colors=data["data"]["Angel of Destiny"][0]["colors"], faceName=None,manaCost=mana_cost)
+card = Card(name=ruin_crab, colors=data["data"]["Angel of Destiny"][0]["colors"], faceName=None,manaCost=data["data"]["Angel of Destiny"][0]["manaCost"])
 
 Environment = Env.Environment()
 engine = Environment.get_database_engine()
--- test_Read_MTGJson.py
+++ test_Read_MTGJson.py
@@ -7,12 +7,3 @@ def test_read_card():
     return ReadCards()
 
 
-def test_mana_cost(test_read_card):
-    assert test_read_card.find_mana_cost_symbol("W", ['W']) == 'W', "Did not find W in ['W']"
-    assert test_read_card.find_mana_cost_symbol("W", ['10', 'W', 'U']) == 'W', "Did not find W in ['10', 'W', 'U']"
-    assert test_read_card.find_mana_cost_symbol("W", ['10', 'W', 'U', 'W']) == 'WW', "Did not find W in ['10', 'W', 'U', 'W']"
-    assert test_read_card.find_mana_cost_symbol("W", ['10', 'W', 'U', 'W']) == 'WW', "Did not find W in ['10', 'W', 'U', 'W']"
-    assert test_read_card.find_mana_cost_symbol("U", ['10', 'W', 'U', 'W']) == 'U', "Did not find U in ['10', 'W', 'U', 'W']"
-    assert test_read_card.find_mana_cost_symbol("C", ['10', 'W', 'U', 'W']) == '', "Did not return blank searching for C in ['10', 'W', 'U', 'W']"
-
-
