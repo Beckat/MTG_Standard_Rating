3d7ec8c7740271587dd108ce95a09c8c78a06a44
danthom1704 <freddrickmacguffin@gmail.com>
2021-08-08 22:08:48 -0400

    Very basic first test of Selenium

--- GetMTGRatings.py
+++ GetMTGRatings.py
@@ -1,9 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlopen, Request
+import urllib
+import json
+import re
+import selenium
 
-req = Request("https://www.coolstuffinc.com/a/aliaintrazi-07222021-afr-standard-set-review-red", headers={"User Agent" : "Mozilla/5.0"})
-page = urlopen(req).read()
-soup = BeautifulSoup(page, 'html.parser')
+import time
 
-print(soup.prettify())
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+
+driver.get('http://www.google.com/');
+
+time.sleep(5) # Let the user actually see something!
+
+search_box = driver.find_element_by_name('q')
+
+search_box.send_keys('ChromeDriver')
+
+search_box.submit()
+
+time.sleep(5) # Let the user actually see something!
+
+driver.quit()
+
+#page = requests.get("https://draftsim.com/STX-pick-order.php")
+#soup = BeautifulSoup(page.text, 'html.parser')
+
+#print(soup.prettify())
+
+#data_list  = soup.find_all("script")
+#data = data_list[142]
+#p = re.compile('construct_pick_order.js = (.*?);')
+#m = p.match(str(data))
+#stocks = json.loads(m.groups()[0])
+
