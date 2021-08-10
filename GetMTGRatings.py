d46c749a1902fd71af4424d3f806fab02b70b495
danthom1704 <freddrickmacguffin@gmail.com>
2021-08-09 23:01:48 -0400

    Pulls the card rankings from the div object

--- GetMTGRatings.py
+++ GetMTGRatings.py
@@ -5,6 +5,7 @@ import urllib
 import json
 import re
 import selenium
+from selenium.webdriver.common.by import By
 
 import time
 
@@ -13,28 +14,9 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
 
-driver.get('http://www.google.com/');
+driver.get('https://draftsim.com/STX-pick-order.php');
 
-time.sleep(5) # Let the user actually see something!
-
-search_box = driver.find_element_by_name('q')
-
-search_box.send_keys('ChromeDriver')
-
-search_box.submit()
-
-time.sleep(5) # Let the user actually see something!
+card_ranks = driver.find_elements(By.ID, 'draft_img')
 
 driver.quit()
-
-#page = requests.get("https://draftsim.com/STX-pick-order.php")
-#soup = BeautifulSoup(page.text, 'html.parser')
-
-#print(soup.prettify())
-
-#data_list  = soup.find_all("script")
-#data = data_list[142]
-#p = re.compile('construct_pick_order.js = (.*?);')
-#m = p.match(str(data))
-#stocks = json.loads(m.groups()[0])
 
