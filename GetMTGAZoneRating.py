f89cb79bcc802e1cbab81fe8589bcf9f2258c65f
danthom1704 <freddrickmacguffin@gmail.com>
2021-08-17 22:36:42 -0400

    Found another site to compare and some initial notes on how to work it

---
+++ GetMTGAZoneRating.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+from urllib.request import urlopen, Request
+import urllib
+import json
+import re
+import selenium
+from selenium.webdriver.common.by import By
+import sqlalchemy as sqlal
+from sqlalchemy.orm import sessionmaker
+import Env
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.exc import IntegrityError
+import time
+from selenium import webdriver
+
+
+# each setp is .077 up from F to A+
+# class=wp-block-columns
+# https://mtgazone.com/strixhaven-school-of-mages-stx-limited-tier-list/
+
+
