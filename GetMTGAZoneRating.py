d94b25cd069f787a1ec7b2168bc24f9cd393fde6
danthom1704 <freddrickmacguffin@gmail.com>
2021-08-21 23:47:47 -0400

    Have grabbed the ratings on Strixhaven from MTGAZone, now need to work out how to parse it

--- GetMTGAZoneRating.py
+++ GetMTGAZoneRating.py
@@ -28,4 +28,13 @@ engine = Environment.get_database_engine()
 Session = sessionmaker(bind=engine)
 database_session = Session()
 
+driver = webdriver.Chrome()
+driver.get("https://mtgazone.com/strixhaven-school-of-mages-stx-limited-tier-list/")
+card_ranks_page = driver.find_elements(By.CLASS_NAME, 'wp-block-columns')
+card_ranks_text = card_ranks_page[0].text
+
+print(card_ranks_text)
+
+driver.quit()
+database_session.close()
 # Added grade breakdown in database
