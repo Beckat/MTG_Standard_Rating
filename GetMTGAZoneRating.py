63c98d184ff3be50c99596ca72d8e77979436006
danthom1704 <freddrickmacguffin@gmail.com>
2021-08-22 23:31:12 -0400

    Created a dictionary of grades from the database

--- GetMTGAZoneRating.py
+++ GetMTGAZoneRating.py
@@ -14,6 +14,7 @@ from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.exc import IntegrityError
 import time
 from selenium import webdriver
+import  pandas as pd
 
 
 # each setp is .077 up from F to A+
@@ -28,10 +29,24 @@ engine = Environment.get_database_engine()
 Session = sessionmaker(bind=engine)
 database_session = Session()
 
+grade_table_df = pd.read_sql_table(
+    "GradeSystemWeight",
+    con=engine
+)
+
+
+trans_df = grade_table_df.set_index("Grade").T
+
+grade_dict = trans_df.to_dict()
+
 driver = webdriver.Chrome()
 driver.get("https://mtgazone.com/strixhaven-school-of-mages-stx-limited-tier-list/")
 card_ranks_page = driver.find_elements(By.CLASS_NAME, 'wp-block-columns')
 card_ranks_text = card_ranks_page[0].text
+card_ranks_split = card_ranks_text.split('\n')
+
+# Check the last two characters against our ratings A+, A, A-, etc... and if it matches remove the last two characters
+# Trim the results and set naem as the remainder and the score as what was there
 
 print(card_ranks_text)
 
